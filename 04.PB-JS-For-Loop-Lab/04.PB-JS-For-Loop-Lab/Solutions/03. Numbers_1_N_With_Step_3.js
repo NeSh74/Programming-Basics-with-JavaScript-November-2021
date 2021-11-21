538d2480101db6eb6dcad8d468fed29063673673
NeSh74 <nejdie@yahoo.com>
2021-11-21 21:02:42 +0200

    add

---
+++ 04.PB-JS-For-Loop-Lab/04.PB-JS-For-Loop-Lab/Solutions/03. Numbers_1_N_With_Step_3.js
@@ -0,0 +1,8 @@
+function numbersBy3(input) {
+    let n = Number(input[0]);
+
+    for (let i = 1; i <= n; i += 3) {
+        console.log(i);
+    }
+}
+numbersBy3(["10"])
---
+++ 04.PB-JS-For-Loop-Lab/04.PB-JS-For-Loop-Lab/Solutions/08NumbersDivisibleBy9.js
@@ -0,0 +1,15 @@
+function divisibleNum(input) {
+    let n = Number(input[0]);
+    let m = Number(input[1]);
+    let sum = 0;
+    let outputNumbers = '';
+    for (let i = n; i < m; i++) {
+        if (i % 9 === 0) {
+            sum += i;
+            outputNumbers += i + '\n';
+        }
+    }
+    console.log("The sum: " + sum);
+    console.log(outputNumbers);
+}
+divisibleNum(["100", "200"]);
